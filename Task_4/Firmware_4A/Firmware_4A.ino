00732261d6ac1c7d4a0233ef2ddec33485486a27
vrnimje <vrnimje_b21@ce.vjti.ac.in>
2023-01-30 11:41:02 +0530

    Added required comments

--- Task_4/Firmware_4A/Firmware_4A.ino
+++ Task_4/Firmware_4A/Firmware_4A.ino
@@ -5,13 +5,36 @@
 
 
 MPU6050 mpu(Wire);
-float x=0.0;
+
+
+// x: Angular deviation (roll) obtained from MPU6050, in degrees
+float x=0.0; 
+
+// x1: Angular deviation in radians
+float x1 = 0.0;
+
+// prev_x: Angular deviation of DB in previous timestep
 float prev_x = 0.0;
+
+// x_dot: Angular velocity of DB
 float x_dot = 0.0;
+
+//Variables for determining time step
+// curr_time: Time in milliseconds from when the board started
+// prev_time: 'curr_time' in previous time step
+// elapsed_time: Time step
 float prev_time = 0, curr_time = 0, elapsed_time = 0;
+
+//pwm: PWM frequency for NIDEC motor
 float pwm = 0;
+
+//prev_pwm: PWM frequency of previous time step
 float prev_pwm = 0;
+
+//sendpwm: PWM frequency to be sent to NIDEC motor, rounded from 'pwm'
 int sendpwm = 0;
+
+//k[4]: K matrix, obtained from Octave modelling 
 float k[4] = {-104.4799, -9.7686, -0.7071, -0.8402};
 
 //Declare NIDEC Motor pins
@@ -19,20 +42,11 @@ float k[4] = {-104.4799, -9.7686, -0.7071, -0.8402};
 #define cw            4  //cw=1, ccw=0
 #define rpm           9  //PWM=255=stop, PWM=0=max_speed  
 
-
-// Create a servo object
-//Servo Servo1;
-//int pos=0;
-//#define servoPin      6   // Declare the Servo pin
-
-
-// Declare the DC Motor Pins
-//#define enA           7
-//#define in1           22
-//#define in2           23
-
-
 /////////////NIDEC Motor//////////////
+
+// Function name: nidec_motor_init()
+// Logic: To initlialise the NIDEC motor
+// Eg call: nidec_motor_init();
 void nidec_motor_init()
 { 
   pinMode(brake, OUTPUT);
@@ -44,6 +58,10 @@ void nidec_motor_init()
   analogWrite(rpm, 255);
 }
 
+// Function name: nidec_motor_control(int pwm)
+// Input: pwm - PWM frequency to be sent to the NIDEC motor
+// Logic: Sends the required PWM to the NIDEC motor
+// Eg call: nidec_motor_control(100);
 void nidec_motor_control(int pwm) 
 { 
   if (pwm < 0) 
@@ -53,6 +71,9 @@ void nidec_motor_control(int pwm)
   analogWrite(rpm, 255 - pwm);
 }
 
+//Function name: nidec_motor_brake()
+//Logic: Stalls the NIDEC motor
+//Eg call: nidec_motor_brake();
 void nidec_motor_brake() 
 { 
   digitalWrite(brake, LOW);  //go=1
@@ -61,6 +82,7 @@ void nidec_motor_brake()
 //////////////////////////////////////
 
 /////////////Timer1 ISR for IMU///////
+// 
 void timer1_init()
 {
     cli(); //Clears the global interrupts
@@ -74,6 +96,7 @@ void timer1_init()
     sei();   //Enables the global interrupts
 }
 
+// Sends timely interrupts to the MPU6050, for sending the angular deviation
 ISR (TIMER1_OVF_vect)
 {
     sei();  
@@ -81,66 +104,12 @@ ISR (TIMER1_OVF_vect)
     TCNT1L = 0x3F; //Counter lower 8 bit value
     mpu.update();
     cli();
-    //prev_x = x;
     x=mpu.getAngleX();
-
-    //Serial.println("ANGLE");
-    //Serial.println(x);
 }
 //////////////////////////////////////
 
-
-/////////////Servo Motor//////////////
-//void servo_init()
-//{
-//  Servo1.attach(servoPin);
-//  pos=90;
-//  Servo1.write(pos);
-//}
-//void servo_move(int nextpos)
-//{ 
-//  if(pos<nextpos)
-//  { for(int i=pos;i<=nextpos;i+=1)
-//    { Servo1.write(i);
-//      delay(10);}
-//  }
-//  else if(pos>nextpos)
-//  { for(int i=pos;i>=nextpos;i-=1)
-//    { Servo1.write(i);
-//      delay(10); }
-//  }
-//  else  { pos=nextpos; }
-//  pos=nextpos;
-//}
-//////////////////////////////////////
-
-
-/////////////DC Motor//////////////
-////void dc_motor_init()
-////{
-////  pinMode(enA, OUTPUT);
-////  pinMode(in1, OUTPUT);
-////  pinMode(in2, OUTPUT);
-//  
-  // Turn off motors - Initial state
-//  digitalWrite(in1, LOW);
-//  digitalWrite(in2, LOW);
-//}
-//void dc_motor_forward(int enablePWM)
-//{
-  //Serial.println(run_rear_motor);
-//  analogWrite(enA, enablePWM);
-//  digitalWrite(in1, LOW);
-//  digitalWrite(in2, HIGH);
-//}
-//void dc_motor_backward(int enablePWM)
-//{
-//  analogWrite(enA, enablePWM);
-//  digitalWrite(in1, HIGH);
-//  digitalWrite(in2, LOW);
-//}
-//////////////////////////////////////
-
+//Function name: setup()
+//Logic: Initialises the MPU6050 and the NIDEC motor
 
 void setup() 
 {
@@ -158,23 +127,23 @@ void setup()
   Serial.println("NIDEC initialized\n");
   timer1_init(); 
   Serial.println("Timer initialized\n");
-  //servo_init();
-  //Serial.println("Servo initialized\n");
-  //dc_motor_init();
-  //Serial.println("DC Motor initialized\n");
 }
-
+// Main loop function
 void loop() 
 {
   curr_time = millis();
   elapsed_time = ((curr_time - prev_time)*0.1);
 
-  float x1 = ((x*3.14159)/180);
-  x_dot = (x1 - prev_x)/elapsed_time;
-  float u = (k[0]*(x1) + k[1]*x_dot);
+
+  x1 = ((x*3.14159)/180); //Converts x (in degrees) to radians
+  x_dot = (x1 - prev_x)/elapsed_time; // Calculation of x_dot (angular deviation rate)
+  float u = (k[0]*(x1) + k[1]*x_dot); // u = -K*x, from LQR controller
+  
   prev_pwm = pwm;
-  pwm = 1.305*u*elapsed_time;
+  pwm = 1.305*u*elapsed_time; // Multiplied to convert required torque to PWM frequency 
   sendpwm = round(pwm);
+
+  //If-else ladder for limiting sent PWM value
   if (sendpwm>255){
     sendpwm = 255;
   }
@@ -182,13 +151,15 @@ void loop()
     sendpwm = -255;
   }
 
+  //Debugging
   Serial.print(x1);
   Serial.print("|");
   Serial.print(x_dot);
   Serial.print("|");
   Serial.print(pwm);
   Serial.print("|");
-  
+
+  //Thresholding PWM values
   if (sendpwm < 10 and sendpwm > 0)
   {
     sendpwm = 10;
@@ -197,9 +168,12 @@ void loop()
   {
     sendpwm = -10;
   }
+  
   nidec_motor_control(sendpwm);
   Serial.print(sendpwm);
   Serial.println("|");
+
+  //Assigning values from previous time step
   prev_time = curr_time;
   prev_x = x1;
   
